302ebb7cbaef927ddefc07006a7ea4bf4f96738f
WuSKai403 <59427281+WuSKai403@users.noreply.github.com>
2025-10-16 13:51:41 +0800

    fix: Correct placement of AutomaticEnv call in LoadConfig function

--- configs/config.go
+++ configs/config.go
@@ -21,12 +21,12 @@ func LoadConfig() {
 	viper.SetConfigName(".env") // Name of config file (without extension)
 	viper.SetConfigType("env")  // REQUIRED if the config file does not have the extension in the name
 
-	viper.AutomaticEnv() // Read in environment variables that match
-
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err != nil {
 		log.Printf("Warning: No .env file found, relying on environment variables: %v", err)
 	}
+
+	viper.AutomaticEnv() // Read in environment variables that match
 
 	if err := viper.Unmarshal(&AppConfig); err != nil {
 		log.Fatalf("Unable to decode config into struct, %v", err)
